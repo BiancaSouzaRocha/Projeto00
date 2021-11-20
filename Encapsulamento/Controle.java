31bb9bbb4832cab2e42fe3868d678fb65960eb07
Bianca <biancasouzarocha@outlook.com>
2021-11-20 16:47:58 -0300

    Projeto Curso em Vídeo

---
+++ Encapsulamento/Controle.java
@@ -0,0 +1,18 @@
+package Encapsulamento;
+
+public class Controle {
+	
+	public static void main (String [] args) {
+		
+		ControleRemoto c = new ControleRemoto ();
+		
+		c.ligar();
+		c.maisVolume();
+		c.ligarMudo();
+		c.abrirMenu();
+		
+		
+		
+	}
+
+}
